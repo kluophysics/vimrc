8fe43f849cd1d6d376cdd39ecde0e0ea65c7f715
Kai Luo <kluophysics@gmail.com>
2019-05-09 11:00:49 -0400

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/env bash
 
 # remove .vim dir and .vimrc file
-cd ~
-rm -irf ~/.vim
-rm -i ~/.vimrc
+#cd ~
+#rm -irf ~/.vim
+#rm -i ~/.vimrc
 
 # assume the vimrc repo has been cloned into ~/.vim. if not do the following:
 #git clone https://github.com/kluophysics/vimrc.git ~/.vim
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -s ~/.vim/vimrc ~/.vimrc
+ln -snf ~/.vim/vimrc ~/.vimrc
 vim +PluginInstall +qall
 
-echo "Done!"
+echo "Setup for vimrc is done!"
 
